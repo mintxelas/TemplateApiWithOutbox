032acd9dfa3db79ef2b0826d8ac0aff6eb750320
mintxelas <fermin.saez@gmail.com>
2020-04-27 22:44:09 +0200

    get messages tests

---
+++ tests/Application/GetAllMessagesHandlerShould.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NSubstitute;
+using Template.Application.GetAllMessages;
+using Template.Domain;
+using Xunit;
+
+namespace Template.Application.Tests
+{
+    public class GetAllMessagesHandlerShould
+    {
+        private readonly IMessageRepository repository;
+        private readonly GetAllMessagesHandler handler;
+        private static readonly Guid SomeId = Guid.NewGuid();
+        private const string SomeText = "some text";
+
+        public GetAllMessagesHandlerShould()
+        {
+            repository = Substitute.For<IMessageRepository>();
+            handler = new GetAllMessagesHandler(repository);
+        }
+
+        [Fact]
+        public async Task retrieve_an_empty_list_if_no_messages_exist()
+        {
+            var response = await handler.Handle(new GetAllMessagesRequest(), CancellationToken.None);
+            Assert.Empty(response.Messages);
+        }
+
+        [Fact]
+        public async Task get_all_database_items_as_list()
+        {
+            var givenMessages = new[] {new Message(SomeId, SomeText)};
+            repository.GetAll().Returns(givenMessages);
+            var response = await handler.Handle(new GetAllMessagesRequest(), CancellationToken.None);
+            Assert.Equal(givenMessages, response.Messages);
+        }
+    }
+}
---
+++ tests/Application/GetMessageByIdShould.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Template.Application.GetAllMessages;
+using Template.Application.GetMessageById;
+using Template.Domain;
+using Xunit;
+
+namespace Template.Application.Tests
+{
+    public class GetMessageByIdShould
+    {
+        private readonly IMessageRepository repository;
+        private readonly GetMessageByIdHandler handler;
+        private static readonly Guid SomeId = Guid.NewGuid();
+        private const string SomeText = "some text";
+
+        public GetMessageByIdShould()
+        {
+            repository = Substitute.For<IMessageRepository>();
+            handler = new GetMessageByIdHandler(repository);
+        }
+
+        [Fact]
+        public async Task return_not_found_when_no_matching_message_is_found()
+        {
+            var response = await handler.Handle(new GetMessageByIdRequest(SomeId), CancellationToken.None);
+            Assert.IsType<MessageByIdNotFoundResponse>(response);
+        }
+
+        [Fact]
+        public async Task return_the_message_that_matches_the_given_id()
+        {
+            var givenMessage = new Message(SomeId, SomeText);
+            repository.GetById(SomeId).Returns(givenMessage);
+            var response = await handler.Handle(new GetMessageByIdRequest(SomeId), CancellationToken.None);
+            Assert.Equal(givenMessage, response.Message);
+        }
+    }
+}
