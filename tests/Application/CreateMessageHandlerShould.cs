553998301487fcceab0e991c2545808200136765
mintxelas <fermin.saez@gmail.com>
2020-04-27 16:48:09 +0200

    add mesage creation and processing handlers' tests

---
+++ tests/Application/CreateMessageHandlerShould.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Template.Application.CreateMessage;
+using Template.Domain;
+using Xunit;
+
+namespace Template.Application.Tests
+{
+    public class CreateMessageHandlerShould
+    {
+        private const string SomeText = "some text";
+        private const string SomeMessage = "some message";
+        private static readonly Guid SomeGuid = Guid.NewGuid();
+        private readonly IMessageRepository repository;
+        private readonly CreateMessageHandler handler;
+
+        public CreateMessageHandlerShould()
+        {
+            repository = Substitute.For<IMessageRepository>();
+            handler = new CreateMessageHandler(repository);
+        }
+
+        [Fact]
+        public async Task return_the_created_message_id_when_successful()
+        {
+            var givenMessage = new Message(SomeText);
+            var expectedMessage = new Message(SomeGuid, SomeText);
+            repository.Save(givenMessage).Returns(expectedMessage);
+            var request = new CreateMessageRequest(SomeText);
+
+            var response = await handler.Handle(request, CancellationToken.None);
+
+            Assert.IsType<CreateMessageSuccessResponse>(response);
+            Assert.Equal(expectedMessage, ((CreateMessageSuccessResponse)response).Message);
+        }
+
+        [Fact]
+        public async Task return_a_generic_message_with_a_description_when_it_fails()
+        {
+            var givenMessage = new Message(SomeText);
+            repository.Save(givenMessage).Throws(new Exception(SomeMessage));
+            var request = new CreateMessageRequest(SomeText);
+
+            var response = await handler.Handle(request, CancellationToken.None);
+
+            Assert.IsNotType<CreateMessageSuccessResponse>(response);
+            Assert.Equal(SomeMessage, response.Description);
+        }
+    }
+}
---
+++ tests/Application/ProcessMessageHandlerShould.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Template.Application.ProcessMessage;
+using Template.Domain;
+using Xunit;
+
+namespace Template.Application.Tests
+{
+    public class ProcessMessageHandlerShould
+    {
+        private const string SomeTextToMatch = "text to match";
+        private const string SomeText = "some text";
+        private const string SomeExceptionMessage = "some exception message";
+        private static readonly Guid SomeId = Guid.NewGuid();
+        private readonly IMessageRepository repository;
+        private readonly ProcessMessageHandler handler;
+
+        public ProcessMessageHandlerShould()
+        {
+            repository = Substitute.For<IMessageRepository>();
+            handler = new ProcessMessageHandler(repository);
+        }
+
+        [Fact]
+        public async Task return_a_sucess_response_when_processing_succeeds()
+        {
+            repository.GetById(SomeId)
+                .Returns(new Message(SomeId, SomeText));
+            var response = await handler.Handle(new ProcessMessageRequest(SomeId, SomeTextToMatch),
+                CancellationToken.None);
+            Assert.Equal("Message processed.", response.Description);
+        }
+
+        [Fact]
+        public async Task return_not_found_response_when_given_message_does_not_exist_in_repository()
+        {
+            var response = await handler.Handle(new ProcessMessageRequest(SomeId, SomeTextToMatch),
+                CancellationToken.None);
+            Assert.IsType<MessageToProcessNotFoundResponse>(response);
+        }
+
+        [Fact]
+        public async Task return_an_error_response_when_an_exception_occurs_during_processing()
+        {
+            repository.GetById(SomeId)
+                .ThrowsForAnyArgs(new Exception(SomeExceptionMessage));
+            var response = await handler.Handle(new ProcessMessageRequest(SomeId, SomeTextToMatch),
+                CancellationToken.None);
+            Assert.IsType<ErrorProcessingMessageResponse>(response);
+            Assert.Equal(SomeExceptionMessage, response.Description);
+        }
+
+    }
+}
