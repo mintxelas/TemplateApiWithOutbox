301f4131a34785c22fc5377a65765a96a9552ede
fermin saez <fsaez@voxelgroup.net>
2025-02-20 16:04:59 +0100

    chore: add references to architecture tests project

---
+++ src/Domain/PlaceHolder.cs
@@ -0,0 +1,6 @@
+namespace Sample.Domain;
+
+public class PlaceHolder
+{
+    
+}
--- tests/Architecture.Tests/ProjectReferenceTests.cs
+++ tests/Architecture.Tests/ProjectReferenceTests.cs
@@ -7,9 +7,9 @@ public class ProjectReferenceTests
     [Fact]
     public void Domain_Should_Not_HaveDependencyOnOtherProjects()
     {
-        var assembly =  typeof(Sample.Domain.AssemblyReference).Assembly;
+        var domainAssembly =  typeof(Sample.Domain.PlaceHolder).Assembly;
         
-        var result = Types.InAssembly(assembly)
+        var result = Types.InAssembly(domainAssembly)
             .Should()
             .NotHaveDependencyOn("Sample.Application")
             .And()
